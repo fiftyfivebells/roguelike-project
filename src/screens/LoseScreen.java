0f35120e463387e1c661fd8fd297ac633dbf7364
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-23 07:28:42 -0400

    Add screen showing lose condition

---
+++ src/screens/LoseScreen.java
@@ -0,0 +1,18 @@
+package screens;
+
+import asciiPanel.AsciiPanel;
+
+import java.awt.event.KeyEvent;
+
+public class LoseScreen implements Screen {
+    @Override
+    public void displayOutput(AsciiPanel terminal) {
+        terminal.write("I think you lost", 1, 1);
+        terminal.writeCenter("-- press [enter] to play again --", 22);
+    }
+
+    @Override
+    public Screen respondToUserInput(KeyEvent key) {
+        return key.getKeyCode() == KeyEvent.VK_ENTER ? new PlayScreen() : this;
+    }
+}
