50f89b757da7fecef0b2db5ce7e2b2cd60c52692
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-22 22:31:44 -0400

    Create screen to show on start

---
+++ src/screens/StartScreen.java
@@ -0,0 +1,15 @@
+package screens;
+
+import java.awt.event.KeyEvent;
+import asciiPanel.AsciiPanel;
+
+public class StartScreen implements Screen {
+    public void displayOutput(AsciiPanel terminal) {
+        terminal.write("roguelike project", 1, 1);
+        terminal.writeCenter("Press [enter] to start the game.", 22);
+    }
+
+    public Screen respondToUserInput(KeyEvent key) {
+        return key.getKeyCode() == KeyEvent.VK_ENTER ? new PlayScreen() : this;
+    }
+}
