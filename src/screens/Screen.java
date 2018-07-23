16f6cb16f6f035fb05a4cb80ecb8b81675cb5067
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-23 07:32:05 -0400

    Remove redundant modifiers

---
+++ src/screens/Screen.java
@@ -0,0 +1,10 @@
+package screens;
+
+import java.awt.event.KeyEvent;
+import asciiPanel.AsciiPanel;
+
+public interface Screen {
+    void displayOutput(AsciiPanel terminal);
+
+    Screen respondToUserInput(KeyEvent key);
+}
