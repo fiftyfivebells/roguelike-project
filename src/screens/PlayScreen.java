ac01029fd5269fb5c111728ca6471eca5e748943
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-23 07:29:48 -0400

    Fix typo in displayOutput method

--- src/screens/PlayScreen.java
+++ src/screens/PlayScreen.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 
 public class PlayScreen implements Screen {
     @Override
-    public void displayOutout(AsciiPanel terminal) {
+    public void displayOutput(AsciiPanel terminal) {
         terminal.write("Look how much fun you're having!", 1, 1);
         terminal.writeCenter(" -- press [escape] to lose or [enter] to win.", 22);
     }
@@ -14,8 +14,8 @@ public class PlayScreen implements Screen {
     @Override
     public Screen respondToUserInput(KeyEvent key) {
         switch(key.getKeyCode()) {
-            case keyEvent.VK_ESCAPE: return new LoseScreen();
-            case keyEvent.VK_ENTER: return new WinScreen();
+            case KeyEvent.VK_ESCAPE: return new LoseScreen();
+            case KeyEvent.VK_ENTER: return new WinScreen();
         }
         return this;
     }
