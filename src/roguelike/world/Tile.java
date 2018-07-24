e35b385e51b6aa1aef90d1a17ba7856edd59c3ba
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-23 21:11:23 -0400

    Create tile enum to build world

--- src/roguelike/world/Tile.java
+++ src/roguelike/world/Tile.java
@@ -1,4 +1,21 @@
 package roguelike.world;
 
-public class Tile {
+import java.awt.Color;
+import asciiPanel.AsciiPanel;
+
+public enum Tile {
+    FLOOR('.', AsciiPanel.white),
+    WALL('#', AsciiPanel.white),
+    BOUNDS('x', AsciiPanel.brightBlack);
+
+    private char glyph;
+    public char getGlyph() { return glyph; }
+
+    private Color color;
+    public Color getColor() { return color; }
+
+    Tile(char glyph, Color color) {
+        this.glyph = glyph;
+        this.color = color;
+    }
 }
