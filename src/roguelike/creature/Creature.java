0308fad2a42ff82113ed6f1770cd77e36f965a75
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-24 13:59:51 -0400

    Add creature ai field and setter

---
+++ src/roguelike/creature/Creature.java
@@ -0,0 +1,42 @@
+package roguelike.creature;
+
+import roguelike.world.World;
+
+import java.awt.Color;
+
+public class Creature {
+    private World world;
+    private CreatureAI ai;
+
+    private int x;
+    private int y;
+
+    private char glyph;
+    private Color color;
+
+    public Creature(World world, char glyph, Color color) {
+        this.world = world;
+        this.glyph = glyph;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getGlyph() {
+        return glyph;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setAi(CreatureAI ai) {
+        this.ai = ai;
+    }
+}
