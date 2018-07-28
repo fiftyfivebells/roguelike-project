7f0e15233573c195d4a4fc96e83ffdc7a605a17d
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-28 17:34:08 -0400

    Create CreatureFactory class

---
+++ src/roguelike/creature/CreatureFactory.java
@@ -0,0 +1,19 @@
+package roguelike.creature;
+
+import asciiPanel.AsciiPanel;
+import roguelike.world.World;
+
+public class CreatureFactory {
+    private World world;
+
+    public CreatureFactory(World world) {
+        this.world = world;
+    }
+
+    public Creature newPlayer() {
+        Creature player = new Creature(world, '@', AsciiPanel.brightWhite);
+        world.addAtEmptyLocation(player);
+        new playerAi(player);
+        return player;
+    }
+}
