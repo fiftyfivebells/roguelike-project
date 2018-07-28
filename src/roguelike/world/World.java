9d6324626cb76e5d9a9d69c0fd48144a98f43a46
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-28 17:37:12 -0400

    Create addAtEmptyLocation method in World

--- src/roguelike/world/World.java
+++ src/roguelike/world/World.java
@@ -1,5 +1,7 @@
 package roguelike.world;
 
+import roguelike.creature.Creature;
+
 import java.awt.Color;
 
 public class World {
@@ -27,6 +29,20 @@ public class World {
         } else {
             return tiles[x][y];
         }
+    }
+
+    public void addAtEmptyLocation(Creature creature) {
+        int x;
+        int y;
+
+        do {
+            x = (int) (Math.random() * width);
+            y = (int) (Math.random() * height);
+        }
+        while (!tile(x, y).isGround());
+
+        creature.setX(x);
+        creature.setY(y);
     }
 
     public char glyph(int x, int y) {
