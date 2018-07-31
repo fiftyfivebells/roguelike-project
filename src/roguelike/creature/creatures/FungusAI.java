66d7008163096a2f5d36b3c77f45f605ce27cd3e
Stephen Bell <stephen.l.bell@gmail.com>
2018-07-30 20:57:57 -0400

    Create FungusAI class

---
+++ src/roguelike/creature/creatures/FungusAI.java
@@ -0,0 +1,10 @@
+package roguelike.creature.creatures;
+
+import roguelike.creature.Creature;
+import roguelike.creature.CreatureAI;
+
+public class FungusAI extends CreatureAI {
+    public FungusAI(Creature creature) {
+        super(creature);
+    }
+}
