614c9c182338b5abebb0a27c51613994b0ac8762
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-02 18:38:21 -0400

    Add doAction method

--- src/roguelike/creature/Creature.java
+++ src/roguelike/creature/Creature.java
@@ -80,8 +80,39 @@ public class Creature {
 
         other.modifyHP(-amount);
 
-        notify("You attack the %s for %d damage.", other.getGlyph(), amount);
-        other.notify("The %s attacks you for %d damage", this.getGlyph(), amount);
+        doAction("attack the %s for %d damage", other.getGlyph(), amount);
+    }
+
+    public void doAction(String message, Object ... params) {
+        int r = 9;
+        for (int ox = -r; ox < r + 1; ox++) {
+            for (int oy = -r; oy < r + 1; oy++) {
+                if (ox * ox + oy * oy > r * r) { continue; }
+
+                Creature other = world.placeCreature(x + ox, y + oy);
+
+                if (other == null) { continue; }
+
+                if (other == this) {
+                    other.notify("You " + message + ".", params);
+                } else {
+                    other.notify(String.format("The %s %s.", getGlyph(), makeSecondPerson(message)), params);
+                }
+            }
+        }
+    }
+
+    private String makeSecondPerson(String text) {
+        String[] words = text.split(" ");
+        words[0] = words[0] + "s";
+
+        StringBuilder builder = new StringBuilder();
+        for (String word : words) {
+            builder.append(" ");
+            builder.append(word);
+        }
+
+        return builder.toString().trim();
     }
 
     public void modifyHP(int amount) {
@@ -113,6 +144,7 @@ public class Creature {
     }
 
     public void notify(String message, Object ... params) {
+
         ai.onNotify(String.format(message, params));
     }
 }
