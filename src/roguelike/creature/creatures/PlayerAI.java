2d38201075b179ef284ea965b9eae1adc96ae146
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-04 17:27:46 -0400

    Override canSee method and create field of view variable

--- src/roguelike/creature/creatures/PlayerAI.java
+++ src/roguelike/creature/creatures/PlayerAI.java
@@ -2,17 +2,26 @@ package roguelike.creature.creatures;
 
 import roguelike.creature.Creature;
 import roguelike.creature.CreatureAI;
+import roguelike.creature.FieldOfView;
 import roguelike.world.Tile;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class PlayerAI extends CreatureAI {
 
     private List<String> messages;
+    private FieldOfView fov;
 
-    public PlayerAI(Creature creature, List<String> messages) {
+    public PlayerAI(Creature creature, List<String> messages, FieldOfView fov) {
         super(creature);
         this.messages = messages;
+        this.fov = fov;
+    }
+
+    @Override
+    public boolean canSee(int wx, int wy, int wz) {
+        return fov.isVisible(wx, wy, wz);
     }
 
     public void onEnter(int x, int y, int z, Tile tile) {
