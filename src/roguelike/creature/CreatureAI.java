1052b3f4d30acfdec73b14f8826e86997aba2b37
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-04 16:48:51 -0400

    Create canSee method in creatureAI

--- src/roguelike/creature/CreatureAI.java
+++ src/roguelike/creature/CreatureAI.java
@@ -1,5 +1,7 @@
 package roguelike.creature;
 
+import roguelike.world.Line;
+import roguelike.world.Point;
 import roguelike.world.Tile;
 
 public class CreatureAI {
@@ -8,6 +10,24 @@ public class CreatureAI {
     public CreatureAI (Creature creature) {
         this.creature = creature;
         this.creature.setAi(this);
+    }
+
+    public boolean canSee(int wx, int wy, int wz) {
+        if (creature.getZ() != wz) {
+            return false;
+        }
+
+        if ((creature.getX()-wx) * (creature.getX()-wx) + (creature.getY()-wy) * (creature.getY()-wy) > creature.getVisionRadius()*creature.getVisionRadius()) {
+            return false
+        }
+
+        for (Point p : new Line(creature.getX(), creature.getY(), wx, wy)) {
+            if (creature.tile(p.getX(), p.getY(), wz).isGround() || p.getX() == wx && p.getY() == wy) {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 
     public void onEnter(int x, int y, int z, Tile tile) {}
