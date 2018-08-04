d5c2998477461a266d2735bcf1611392712b4176
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-04 16:38:25 -0400

    Create new Line class

---
+++ src/roguelike/world/Line.java
@@ -0,0 +1,44 @@
+package roguelike.world;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Line implements Iterable<Point> {
+    private List<Point> points;
+
+    public Line(int x0, int y0, int x1, int y1) {
+        this.points = new ArrayList<Point>();
+
+        int dx = Math.abs(x0 - x1);
+        int dy = Math.abs(y0 - y1);
+
+        int sx = x0 < x1 ? 1 : -1;
+        int sy = y0 < y1 ? 1 : -1;
+        int err = dx - dy;
+
+        while(true) {
+            points.add(new Point(x0, y0, 0));
+
+            if (x0 == x1 && y0 == y1) { break; }
+
+            int e2 = err * 2;
+            if (e2 > -dx) {
+                err -= dy;
+                x0 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public Iterator<Point> iterator() {
+        return points.iterator();
+    }
+}
