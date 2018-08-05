5d3acf899a869fe09aaae9eee883a063b3d24a5b
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-05 16:15:25 -0400

    Make item class

---
+++ src/roguelike/items/Item.java
@@ -0,0 +1,39 @@
+package roguelike.items;
+
+import java.awt.*;
+
+public class Item {
+    private char glyph;
+    private Color color;
+    private String name;
+
+    public Item(char glyph, Color color, String name) {
+        this.glyph = glyph;
+        this.color = color;
+        this.name = name;
+    }
+
+    public char getGlyph() {
+        return glyph;
+    }
+
+    public void setGlyph(char glyph) {
+        this.glyph = glyph;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
