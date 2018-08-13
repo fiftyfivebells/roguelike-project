e7db7666b0590a100dff191b3a7fbfb4999f07bf
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-13 18:51:03 -0400

    Create abstract class representing level-up option

---
+++ src/roguelike/creature/LevelUpOption.java
@@ -0,0 +1,15 @@
+package roguelike.creature;
+
+public abstract class LevelUpOption {
+    private String name;
+
+    public LevelUpOption(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void invoke(Creature creature);
+}
