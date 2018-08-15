cf34db981ae1882b688c910a14f093f98138481e
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-14 21:20:50 -0400

    Make ExamineScreen class to look at inventory

---
+++ src/roguelike/screens/ExamineScreen.java
@@ -0,0 +1,28 @@
+package roguelike.screens;
+
+import roguelike.creature.Creature;
+import roguelike.items.Item;
+
+public class ExamineScreen extends InventoryBasedScreen {
+
+    public ExamineScreen(Creature player) {
+        super(player);
+    }
+
+    @Override
+    protected String getVerb() {
+        return "examine"l
+    }
+
+    @Override
+    protected boolean isAcceptable(Item item) {
+        return true;
+    }
+
+    @Override
+    protected Screen useItem(Item item) {
+        String article = "aeiou".contains(item.getName().subSequence(0, 1)) ? "an " : "a ";
+        player.notify("It's " + article + item.getName() +  "." + item.details());
+        return null;
+    }
+}
