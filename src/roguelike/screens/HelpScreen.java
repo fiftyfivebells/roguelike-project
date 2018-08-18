257173d7c138a77cf44e7520e8a7f6e0e6a90cb5
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-17 22:43:47 -0400

    Add throwing and firing to the help screen

--- src/roguelike/screens/HelpScreen.java
+++ src/roguelike/screens/HelpScreen.java
@@ -17,6 +17,8 @@ public class HelpScreen implements Screen {
         terminal.write("[d] to drop", 2, y++);
         terminal.write("[e] to eat", 2, y++);
         terminal.write("[w] to wear or wield", 2, y++);
+        terminal.write("[f] to fire your weapon", 2, y++);
+        terminal.write("[t] to throw an item", 2, y++);
         terminal.write("[?] for help", 2, y++);
         terminal.write("[x] to examine your items", 2, y++);
         terminal.write("[;] to look around", 2, y++);
