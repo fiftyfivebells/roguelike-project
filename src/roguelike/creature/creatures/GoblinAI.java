0772a5f3ed6f4c91a1354f12bdf3ea23b8d58496
Stephen Bell <stephen.l.bell@gmail.com>
2018-08-18 21:31:51 -0400

    Add GoblinAI class

---
+++ src/roguelike/creature/creatures/GoblinAI.java
@@ -0,0 +1,34 @@
+package roguelike.creature.creatures;
+
+import roguelike.creature.Creature;
+import roguelike.creature.CreatureAI;
+import roguelike.creature.Path;
+import roguelike.world.Point;
+
+import java.util.List;
+
+public class GoblinAI extends CreatureAI {
+    private Creature player;
+
+    public GoblinAI(Creature creature, Creature player) {
+        super(creature);
+        this.player = player;
+    }
+
+    public void onUpdate() {
+        if (creature.canSee(player.getX(), player.getY(), player.getZ())) {
+            hunt(player);
+        } else {
+            wander();
+        }
+    }
+
+    public void hunt(Creature target) {
+        List<Point> points = new Path(creature, target.getX(), target.getY()).getPoints();
+
+        int mx = points.get(0).getX() - creature.getX();
+        int my = points.get(0).getY() - creature.getY();
+
+        creature.moveBy(mx, my, 0);
+    }
+}
